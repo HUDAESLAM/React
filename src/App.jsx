2509038bc20e59cfddd4a46577793c6e6bcc7c83
HUDAESLAM <hudaeslam50555@gmail.com>
2025-07-24 00:35:08 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 // import { useState } from 'react'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./Layout/Layout";
-import  Home  from "./Home/Home";
-import  About  from "./About/About";
-import  Portfolio  from "./Portfolio/Portfolio";
-import  Contact  from "./Contact/Contact";
+import Home from "./Home/Home";
+import About from "./About/About";
+import Portfolio from "./Portfolio/Portfolio";
+import Contact from "./Contact/Contact";
 import ErrorPage from "./Error/ErrorPage";
 
 const routers = createBrowserRouter([
@@ -12,11 +12,11 @@ const routers = createBrowserRouter([
     path: "/",
     element: <Layout />,
     children: [
-      { index: true, element: <Home /> },
+      { index: true, path: "/React", element: <Home /> },
       { path: "/about", element: <About /> },
       { path: "/portfolio", element: <Portfolio /> },
       { path: "/contact", element: <Contact /> },
-      {path: "*", element: <ErrorPage /> } 
+      { path: "*", element: <ErrorPage /> },
     ],
   },
 ]);
